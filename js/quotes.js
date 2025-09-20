1c788f1f3486297dd10fb4797673ed06a00eee7a
pasindubalasooriya <pasindudilshanbalasooriya@gmail.com>
2025-09-20 18:19:19 +0530

    add navigation bar

--- js/quotes.js
+++ js/quotes.js
@@ -1,14 +1,14 @@
 const healthQuotes = [
-  "The greatest wealth is health.",
-  "Your health is an investment, not an expense.",
-  "The first wealth is health.",
-  "Take care of your body. It's the only place you have to live.",
-  "Health is not valued till sickness comes.",
-  "Let food be thy medicine, and medicine be thy food.",
-  "Your body hears everything your mind says.",
-  "Healthy living is a journey, not a destination.",
-  "A healthy outside starts from the inside.",
-  "Good health is not something we can buy.",
+  "The greatest wealth is health...",
+  "Your health is an investment, not an expense...",
+  "The first wealth is health...",
+  "Take care of your body. It's the only place you have to live...",
+  "Health is not valued till sickness comes...",
+  "Let food be thy medicine, and medicine be thy food...",
+  "Your body hears everything your mind says...",
+  "Healthy living is a journey, not a destination...",
+  "A healthy outside starts from the inside...",
+  "Good health is not something we can buy...",
 ];
 
 let i = 0;
